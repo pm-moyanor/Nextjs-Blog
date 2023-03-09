2f85df4c5f99c063df802d4c0bda34cdd77b7ad6
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-03-09 10:14:40 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,42 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
+// check for Layouts in docs
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter;
+  return (
+    <>
+      <div>
+        <h1>Site</h1>
+        <nav className="header-nav">
+          <ul>
+            <li>
+              <Link className={router.pathname == "/" ? "active" : ""} href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={router.pathname == "/About" ? "active" : ""}
+                href="/About"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={router.pathname == "/Blog" ? "active" : ""}
+                href="/Blog"
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <p>Header</p>
+      <Component {...pageProps} />
+    </>
+  );
 }
